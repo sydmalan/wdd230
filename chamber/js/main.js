fe21b24ac245598d896b9ce1c1fdc8a6651033d0
sydmalan <mal19002@byui.edu>
2022-10-10 06:11:45 -0600

    creating site

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -1,2 +1,29 @@
+// const date_now = new Date();
+// const full_date = new Intl.DateTimeFormat('en-us', {dateStyle: 'full'}).format(date._now);
+
+//header
 const date_now = new Date();
-const full_date = new Intl.DateTimeFormat('en-us', {dateStyle: 'full'}).format(date._now);
+
+const full_date = new Intl.DateTimeFormat('en-us', {dateStyle: 'full'}).format(date_now);
+const current_date_element = document.querySelector("#current-date");
+current_date_element.innerHTML = date_now;
+
+
+function toggleMenu(){
+    document.getElementById('primarynav').classList.toggle('open');
+    document.getElementById('hambutton').classList.toggle('open');
+}
+
+const x = document.getElementById('hambutton')
+x.onclick = toggleMenu;
+
+
+//footer
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').textContent = year;
+
+//shows the date the site was last modified 
+let currentdate = document.lastModified;
+document.querySelector('.updated').textContent = currentdate;
